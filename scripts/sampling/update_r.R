9efcd90a7f45a1e631637bfc39ec3df843dce41a
Geert Huissen <g.j.huissen@tilburguniversity.edu>
2025-10-08 15:09:43 +0200

    Removed the scripts folder from repo, and will replace it with src in the next push

--- scripts/sampling/update_r.R
+++
@@ -1,9 +0,0 @@
-# Extract the R code from sampling.Rmd into sampling.R
-
-knitr::purl(
-	input  = "sampling.Rmd",
-	output = "sampling.R",
-	documentation = 0
-)
-
-message("Sampling.R updated from sampling.Rmd")
